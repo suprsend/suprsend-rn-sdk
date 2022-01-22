ba384f2e4ca8b1384b04d09082de9892c71458bb
sivaram000 <katta.sivaram@suprsend.com>
2022-01-22 11:20:01 +0530

    added suprsend methods in custom module

--- android/src/main/java/com/suprsendrnsdk/SuprsendRnSdkModule.java
+++ android/src/main/java/com/suprsendrnsdk/SuprsendRnSdkModule.java
@@ -1,57 +1,303 @@
 package com.suprsendrnsdk;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.module.annotations.ReactModule;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import app.suprsend.SSApi;
 import app.suprsend.base.LogLevel;
 
 
 @ReactModule(name = SuprsendRnSdkModule.NAME)
 public class SuprsendRnSdkModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "SuprsendRnSdk";
-    private final ReactApplicationContext context;
-    private SSApi suprsendInstance;
-    private String apiKey, apiSecret, apiBaseUrl;
+  public static final String NAME = "SuprsendRnSdk";
+  private final ReactApplicationContext context;
+  private SSApi suprsendInstance;
+  private String apiKey, apiSecret, apiBaseUrl;
 
-    public SuprsendRnSdkModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.context = reactContext;
+  public SuprsendRnSdkModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.context = reactContext;
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  private void getInstance() {
+    if (suprsendInstance == null) {
+      suprsendInstance = SSApi.Companion.getInstance(apiKey, apiSecret, apiBaseUrl);
+      suprsendInstance.setLogLevel(LogLevel.VERBOSE);
     }
+  }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
-
-    private void getInstance(){
-        if(suprsendInstance==null){
-          suprsendInstance = SSApi.Companion.getInstance(apiKey, apiSecret, apiBaseUrl);
-          suprsendInstance.setLogLevel(LogLevel.VERBOSE);
-        }
-    }
-
-    @ReactMethod
-    public void initializeSDK(String workspaceKey, String workspaceSecret, String apiUrl){
-      apiKey = workspaceKey;
-      apiSecret = workspaceSecret;
+  @ReactMethod
+  public void initializeSDK(String workspaceKey, String workspaceSecret, String apiUrl) {
+    apiKey = workspaceKey;
+    apiSecret = workspaceSecret;
+    if (apiUrl != null) {
       apiBaseUrl = apiUrl;
     }
+  }
 
-    @ReactMethod
-    public void identify(String uniqueId) {
-        getInstance();
-        suprsendInstance.identify(uniqueId);
-    }
+  @ReactMethod
+  public void identify(String uniqueId) {
+    getInstance();
+    suprsendInstance.identify(uniqueId);
+  }
 
-    @ReactMethod
-    public void reset() {
-      getInstance();
-      suprsendInstance.reset();
+  @ReactMethod
+  public void set(ReadableMap properties) {
+    if (properties == null) {
+      return;
     }
+    getInstance();
+    JSONObject jsonData = convertMapToJson(properties);
+    suprsendInstance.getUser().set(jsonData);
+  }
+
+  @ReactMethod
+  public void setOnce(ReadableMap properties) {
+    if (properties == null) {
+      return;
+    }
+    getInstance();
+    JSONObject jsonData = convertMapToJson(properties);
+    suprsendInstance.getUser().setOnce(jsonData);
+  }
+
+  @ReactMethod
+  public void increment(ReadableMap properties) {
+    if (properties == null) {
+      return;
+    }
+    getInstance();
+    Map<String, Number> formattedMap = new HashMap<String, Number>();
+    ReadableMapKeySetIterator iterator = properties.keySetIterator();
+    while (iterator.hasNextKey()) {
+      String key = iterator.nextKey();
+      switch (properties.getType(key)) {
+        case Number:
+          formattedMap.put(key, properties.getDouble(key));
+          break;
+      }
+    }
+    suprsendInstance.getUser().increment(formattedMap);
+  }
+
+  @ReactMethod
+  public void append(ReadableMap properties) {
+    if (properties == null) {
+      return;
+    }
+    getInstance();
+    JSONObject jsonData = convertMapToJson(properties);
+    suprsendInstance.getUser().append(jsonData);
+  }
+
+  @ReactMethod
+  public void remove(ReadableMap properties) {
+    if (properties == null) {
+      return;
+    }
+    getInstance();
+    JSONObject jsonData = convertMapToJson(properties);
+    suprsendInstance.getUser().remove(jsonData);
+  }
+
+  @ReactMethod
+  public void unSet(ReadableArray keys) {
+    if (keys == null) {
+      return;
+    }
+    getInstance();
+    List<String> formattedList = new ArrayList<String>();
+    for (int i = 0; i < keys.size(); i++) {
+      switch (keys.getType(i)) {
+        case String:
+          formattedList.add(keys.getString(i));
+          break;
+      }
+    }
+    suprsendInstance.getUser().unSet(formattedList);
+  }
+
+  @ReactMethod
+  public void reset() {
+    getInstance();
+    suprsendInstance.reset();
+  }
+
+  @ReactMethod
+  public void setEmail(String email) {
+    getInstance();
+    suprsendInstance.getUser().setEmail(email);
+  }
+
+  @ReactMethod
+  public void unSetEmail(String email) {
+    getInstance();
+    suprsendInstance.getUser().unSetEmail(email);
+  }
+
+  @ReactMethod
+  public void setSms(String mobile) {
+    getInstance();
+    suprsendInstance.getUser().setSms(mobile);
+  }
+
+  @ReactMethod
+  public void unSetSms(String mobile) {
+    getInstance();
+    suprsendInstance.getUser().unSetEmail(mobile);
+  }
+
+  @ReactMethod
+  public void setWhatsApp(String mobile) {
+    getInstance();
+    suprsendInstance.getUser().setWhatsApp(mobile);
+  }
+
+  @ReactMethod
+  public void unSetWhatsApp(String mobile) {
+    getInstance();
+    suprsendInstance.getUser().unSetWhatsApp(mobile);
+  }
+
+  @ReactMethod
+  public void setAndroidFcmPush(String token) {
+    getInstance();
+    suprsendInstance.getUser().setAndroidFcmPush(token);
+  }
+
+  @ReactMethod
+  public void unSetAndroidFcmPush(String token) {
+    getInstance();
+    suprsendInstance.getUser().unSetAndroidFcmPush(token);
+  }
+
+  @ReactMethod
+  public void setAndroidXiaomiPush(String token) {
+    getInstance();
+    suprsendInstance.getUser().setAndroidXiaomiPush(token);
+  }
+
+  @ReactMethod
+  public void unSetAndroidXiaomiPush(String token) {
+    getInstance();
+    suprsendInstance.getUser().unSetAndroidXiaomiPush(token);
+  }
+
+  @ReactMethod
+  public void track(String eventName, ReadableMap properties) {
+    getInstance();
+    JSONObject formattedProperties = convertMapToJson(properties);
+    suprsendInstance.track(eventName, formattedProperties);
+  }
+
+  @ReactMethod
+  public void setSuperProperties(ReadableMap properties) {
+    getInstance();
+    JSONObject jsonData = convertMapToJson(properties);
+    suprsendInstance.setSuperProperties(jsonData);
+  }
+
+  @ReactMethod
+  public void unSetSuperProperty(String key) {
+    getInstance();
+    suprsendInstance.unSetSuperProperty(key);
+  }
+
+  @ReactMethod
+  public void flush() {
+    getInstance();
+    suprsendInstance.flush();
+  }
+
+  private static JSONObject convertMapToJson(ReadableMap readableMap) {
+    if (readableMap == null) {
+      return null;
+    }
+    JSONObject object = new JSONObject();
+    ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+    while (iterator.hasNextKey()) {
+      String key = iterator.nextKey();
+      try {
+        switch (readableMap.getType(key)) {
+          case Null:
+            object.put(key, JSONObject.NULL);
+            break;
+          case Boolean:
+            object.put(key, readableMap.getBoolean(key));
+            break;
+          case Number:
+            object.put(key, readableMap.getDouble(key));
+            break;
+          case String:
+            object.put(key, readableMap.getString(key));
+            break;
+          case Map:
+            object.put(key, convertMapToJson(readableMap.getMap(key)));
+            break;
+          case Array:
+            object.put(key, convertArrayToJson(readableMap.getArray(key)));
+            break;
+        }
+      } catch (Exception e) {
+        Log.d("SS_ERROR", "error is " + e);
+      }
+    }
+    return object;
+  }
+
+  private static JSONArray convertArrayToJson(ReadableArray readableArray) {
+    if (readableArray == null) {
+      return null;
+    }
+    JSONArray array = new JSONArray();
+    for (int i = 0; i < readableArray.size(); i++) {
+      try {
+        switch (readableArray.getType(i)) {
+          case Null:
+            break;
+          case Boolean:
+            array.put(readableArray.getBoolean(i));
+            break;
+          case Number:
+            array.put(readableArray.getDouble(i));
+            break;
+          case String:
+            array.put(readableArray.getString(i));
+            break;
+          case Map:
+            array.put(convertMapToJson(readableArray.getMap(i)));
+            break;
+          case Array:
+            array.put(convertArrayToJson(readableArray.getArray(i)));
+            break;
+        }
+      } catch (Exception e) {
+        Log.d("SS_ERROR", "error is " + e);
+      }
+    }
+    return array;
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,72 @@ import { NativeModules } from 'react-native';
 
 const SuprsendRnSdk = NativeModules.SuprsendRnSdk;
 
+const user = {
+  set: function (properties: Object) {
+    SuprsendRnSdk.set(properties);
+  },
+
+  setOnce: function (properties: Object) {
+    SuprsendRnSdk.setOnce(properties);
+  },
+
+  increment: function (properties: { [key: string]: number }) {
+    SuprsendRnSdk.increment(properties);
+  },
+
+  append: function (properties: Object) {
+    SuprsendRnSdk.append(properties);
+  },
+
+  remove: function (properties: Object) {
+    SuprsendRnSdk.remove(properties);
+  },
+
+  unSet: function (keys: string[]) {
+    SuprsendRnSdk.unSet(keys);
+  },
+  setEmail: function (email: string) {
+    SuprsendRnSdk.setEmail(email);
+  },
+
+  unSetEmail: function (email: string) {
+    SuprsendRnSdk.unSetEmail(email);
+  },
+
+  setSms: function (mobile: string) {
+    SuprsendRnSdk.setSms(mobile);
+  },
+
+  unSetSms: function (mobile: string) {
+    SuprsendRnSdk.unSetSms(mobile);
+  },
+
+  setWhatsApp: function (mobile: string) {
+    SuprsendRnSdk.setWhatsApp(mobile);
+  },
+
+  unSetWhatsApp: function (mobile: string) {
+    SuprsendRnSdk.unSetWhatsApp(mobile);
+  },
+
+  setAndroidFcmPush: function (token: string) {
+    SuprsendRnSdk.setAndroidFcmPush(token);
+  },
+
+  unSetAndroidFcmPush: function (token: string) {
+    SuprsendRnSdk.unSetAndroidFcmPush(token);
+  },
+
+  setAndroidXiaomiPush: function (token: string) {
+    SuprsendRnSdk.setAndroidXiaomiPush(token);
+  },
+
+  unSetAndroidXiaomiPush: function (token: string) {
+    SuprsendRnSdk.unSetAndroidXiaomiPush(token);
+  },
+};
+
 const Suprsend = {
-  // method to initialize sdk with given key and secret
   initializeSDK: function (
     workspace_key: string,
     workspace_secret: string,
@@ -12,12 +76,28 @@ const Suprsend = {
     SuprsendRnSdk.initializeSDK(workspace_key, workspace_secret, base_url);
   },
 
-  // method to identify the user
   identify: function (unique_id: any) {
     SuprsendRnSdk.identify(unique_id);
   },
 
-  // method to reset user
+  user: user,
+
+  track: function (eventName: string, properties?: Object) {
+    SuprsendRnSdk.track(eventName, properties);
+  },
+
+  setSuperProperties: function (properties: Object) {
+    SuprsendRnSdk.setSuperProperties(properties);
+  },
+
+  unSetSuperProperty: function (key: string) {
+    SuprsendRnSdk.unSetSuperProperty(key);
+  },
+
+  flush: function () {
+    SuprsendRnSdk.flush();
+  },
+
   reset: function () {
     SuprsendRnSdk.reset();
   },
